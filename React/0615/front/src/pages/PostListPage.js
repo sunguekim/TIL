eea6da1ebbf63ab35955de841af35554f55d0ba7
sunguekim <sunguekim96@gmail.com>
2020-06-30 17:52:16 +0900

    react0630/Header 컴포넌트 생성

--- React/0615/front/src/pages/PostListPage.js
+++ React/0615/front/src/pages/PostListPage.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import Button from '../components/common/Button'
+import Header from '../components/common/Header'
 
-const PostListPage=()=>{
-    return(
+const PostListPage = () => {
+    return (
         <div>
-            <Button>버튼</Button>
+            <Header />
+            <div>hihi</div>
         </div>
     )
 }
